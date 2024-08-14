da619c250320d0daa61e0851aecca31e8d1c9b40
Polina Medvedeva <medvedevapolina84@gmail.com>
2024-08-14 23:39:49 +0300

    Sprint_6: fix LionParametrizedTest

--- src/test/java/LionParametrizedTest.java
+++ src/test/java/LionParametrizedTest.java
@@ -1,37 +1,47 @@
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 @RunWith(Parameterized.class)
 public class LionParametrizedTest {
 
-    String sex;
-    boolean hasMane;
+    private Feline feline;
+    private String sex;
+    private boolean hasMane;
 
     public LionParametrizedTest(String sex, boolean hasMane) {
         this.sex = sex;
         this.hasMane = hasMane;
     }
 
+    @Before
+    public void setUp() {
+        feline = mock(Feline.class); // Создаем мок для Feline
+    }
+
     @Parameterized.Parameters
-    public static Object[][] testData() {
-        return new Object[][]{
+    public static Object[][] data() {
+        return new Object[][] {
                 {"Самец", true},
-                {"Самка", false},
-                {"Лев", false}
+                {"Самка", false}
         };
     }
 
     @Test
-    public void doesHasManeTest() {
-        try {
-            Lion lion = new Lion(new Feline(), sex);
-            Assert.assertEquals(hasMane, lion.doesHaveMane());
-        } catch (Exception e) {
-            Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
-        }
+    public void testConstructorWithValidSex() throws Exception {
+        Lion lion = new Lion(feline, sex);
+        assertEquals(hasMane, lion.doesHaveMane());
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithInvalidSex() throws Exception {
+        new Lion(feline, "Неизвестный пол");
     }
 }
